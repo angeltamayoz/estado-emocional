4483620c19acc2d29926f890a136a57af27f73e1
Johan Velez <johanstiven135@gmail.com>
2025-09-29 01:05:57 -0500

    Se crea la simulación del registro de usuarios

---
+++ src/app/__init__.py
@@ -0,0 +1,24 @@
+# __init__.py
+
+from utils import registrar_usuario, mostrar_usuarios
+
+def main():
+    while True:
+        print("1. Registrar nuevo usuario")
+        print("2. Ver usuarios registrados")
+        print("3. Salir")
+        
+        opcion = input("Selecciona una opción: ")
+        
+        if opcion == "1":
+            registrar_usuario()
+        elif opcion == "2":
+            mostrar_usuarios()
+        elif opcion == "3":
+            print("¡Hasta luego!")
+            break
+        else:
+            print("Opción inválida. Intenta de nuevo.\n")
+
+if __name__ == "__main__":
+    main()
